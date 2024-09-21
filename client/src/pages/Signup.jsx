381eb7b09e6f997b18ac5e491c762c7c25facb14
RounokRabbi <rounok.cse.20220104096@aust.edu>
2024-09-21 19:19:33 +0600

    signup update

--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -23,7 +23,7 @@ const Signup = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const res = await axios.post(`/api/auth/signup`, formData);
+      const res = await axios.post(`https://travel-management-api-five.vercel.app/api/auth/signup`, formData);
       if (res?.data?.success) {
         alert(res?.data?.message);
         navigate("/login");
