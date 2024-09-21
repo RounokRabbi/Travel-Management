da5139a1b2a7613ceacb57f706c2a6ee467597fb
RounokRabbi <rounok.cse.20220104096@aust.edu>
2024-09-21 20:25:44 +0600

    update

--- backend/middlewares/authMiddleware.js
+++ backend/middlewares/authMiddleware.js
@@ -2,30 +2,39 @@ import jwt from "jsonwebtoken";
 import User from "../models/user.model.js";
 
 export const requireSignIn = async (req, res, next) => {
-  if (req?.cookies?.X_TTMS_access_token) {
-    const token = await req.cookies.X_TTMS_access_token;
-    if (!token)
-      return res.status(401).send({
-        success: false,
-        message: "Unautorized: Token not provided!",
-      });
-
-    jwt.verify(token, 'ASDFGHJKL:1234567890465789dsgsdgsdgdsgdsgsdgds', (err, user) => {
-      if (err)
-        return res.status(403).send({
-          success: false,
-          message: "Forbidden",
-        });
-
-      req.user = user;
-      next();
-    });
-  } else {
-    return res.status(401).send({
-      success: false,
-      message: "Unautorized: Token not provided!",
-    });
+  let token
+  let user=req.user
+  if (req.cookies.jwt ){
+token= req.cookies.jwt
+await jwt.verify(token, 'ASDFGHJKL:1234567890465789dsgsdgsdgdsgdsgsdgds')
+res.locals.user=user
+return res.status(201).json{"success"}
   }
+  
+  // if (req?.cookies?.X_TTMS_access_token) {
+  //   const token = await req.cookies.X_TTMS_access_token;
+  //   if (!token)
+  //     return res.status(401).send({
+  //       success: false,
+  //       message: "Unautorized: Token not provided!",
+  //     });
+
+  //   jwt.verify(token, 'ASDFGHJKL:1234567890465789dsgsdgsdgdsgdsgsdgds', (err, user) => {
+  //     if (err)
+  //       return res.status(403).send({
+  //         success: false,
+  //         message: "Forbidden",
+  //       });
+
+  //     req.user = user;
+  //     next();
+  //   });
+  // } else {
+    // return res.status(401).send({
+    //   success: false,
+    //   message: "Unautorized: Token not provided!",
+    // });
+  // }
 };
 
 //Admin access
